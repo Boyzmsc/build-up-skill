fd68af5cb206f7a07a6ead0aa2c3de505d42208e
Boyzmsc <boyzmsc@naver.com>
2021-01-16 14:17:19 +0900

    Complete 15552 & 2741

--- for-statement/print-n.cpp
+++ for-statement/print-n.cpp
@@ -6,7 +6,18 @@ using namespace std;
 int getTestCaseNum()
 {
     int t;
-    cin >> t;
+    while (true)
+    {
+        cin >> t;
+        if ((t > 0) && (t <= 1000000))
+        {
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
     return t;
 }
 
@@ -18,7 +29,7 @@ void getPlusTable(int testNum)
         cin >> a >> b;
         while (true)
         {
-            if ((a > 0) && (b < 10))
+            if ((a >= 1) && (a <= 1000) && (b >= 1) && (b <= 1000))
             {
                 break;
             }
@@ -27,12 +38,16 @@ void getPlusTable(int testNum)
                 continue;
             }
         }
-        cout << a + b << endl;
+        cout << a + b << "\n";
     }
 }
 
 int main()
 {
+    // C++의 표준 stream의 동기화를 끊는 역할
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
     int testCase = getTestCaseNum();
     getPlusTable(testCase);
 }
