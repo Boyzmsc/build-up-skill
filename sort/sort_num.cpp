aff64f4b19b5e709a8871a31578e18c11bfbfc6f
Boyzmsc <boyzmsc@naver.com>
2021-01-23 22:14:45 +0900

    Complete 2750

---
+++ sort/sort_num.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+void sortNum(int n)
+{
+    vector<int> nums;
+    while (n--)
+    {
+        int num;
+        cin >> num;
+        nums.push_back(num);
+    }
+
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[i] >= nums[j])
+            {
+                int tmp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+    }
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << '\n';
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    sortNum(n);
+}
