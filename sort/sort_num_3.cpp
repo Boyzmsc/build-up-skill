853fdb5c7771024802b00d974c88485680c8d99d
Boyzmsc <boyzmsc@naver.com>
2021-01-23 23:25:15 +0900

    feat: Add comments

--- sort/sort_num_3.cpp
+++ sort/sort_num_3.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+// sort_num_3.cpp를 하면서 느낀점!
+// cin,cout보다 scanf,printf가 출력이 더 빠른 것 같음
+// 해당 정렬은 메모리와 시간 제한을 걸어두었기 때문에
+// 최대한 간단하게 구현하고자 main()문 안에 모두 구현
+// Counting Sort를 활용한 케이스
 
 int arr[10001];
 
