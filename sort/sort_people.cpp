fdf3e8a7339604d21f906b2d7a302f6b954e6487
Boyzmsc <boyzmsc@naver.com>
2021-01-24 13:59:43 +0900

    Complete phase-12

--- sort/sort_people.cpp
+++ sort/sort_people.cpp
@@ -19,6 +19,8 @@ public:
     }
 };
 
+// sort 함수 세번째 인자에 넣어줄 비교 함수
+// 두 Person 객체를 비교함으로써 true와 false 값을 리턴
 bool cmp(const Person &p1, const Person &p2)
 {
     if (p1.index < p2.index){
@@ -42,6 +44,7 @@ void sortPeople(int n)
 
     // sort 함수 세번째 인자에 비교 함수를 넣어줌으로써
     // 해당 함수의 정의된 조건을 기준으로 정렬
+    // 추가로 stable_sort를 이용함으로써 기존 배열에서의 순서 유지
     stable_sort(people.begin(), people.end(), cmp);
 
     for (int i = 0; i < people.size(); i++)
