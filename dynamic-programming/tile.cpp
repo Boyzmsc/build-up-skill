cf90bdff588b066a8d11adc31f572369452b7257
Boyzmsc <boyzmsc@naver.com>
2021-01-31 23:52:28 +0900

    Complete 1904

--- dynamic-programming/tile.cpp
+++ dynamic-programming/tile.cpp
@@ -4,59 +4,16 @@
 
 using namespace std;
 
-int len;
-int cases = 0;
-vector<string> tiles;
-string tile;
-
-void getTileCases(int count)
-{
-    if (count > len)
-    {
-        return;
-    }
-    if (count == len)
-    {
-        cases++;
-        return;
-    }
-    for (int i = 0; i <= 1; i++)
-    {
-        if (i == 0)
-        {
-            tile.append("00");
-
-            getTileCases(count + 2);
-            tile = tile.substr(0, tile.length() - 2);
-        }
-        else
-        {
-            tile.append("1");
-
-            getTileCases(count + 1);
-            tile = tile.substr(0, tile.length() - 1);
-        }
-    }
-}
-
 int main()
 {
+    int len;
     cin >> len;
-    for (int i = 0; i < len; i++)
+    int cases[len + 1];
+    cases[0] = 1;
+    cases[1] = 1;
+    for (int i = 2; i <= len; i++)
     {
-        tiles.push_back("1");
+        cases[i] = (cases[i - 1] + cases[i - 2]) % 15746;
     }
-    for (int i = 0; i < len / 2; i++)
-    {
-        tiles.push_back("00");
-    }
-    // for (int j = 0; j < tiles.size(); j++)
-    // {
-    //     cout << tiles[j] << " ";
-    // }
-    // cout << endl;
-
-    getTileCases(0);
-    // cout << cases << endl;
-    cout << cases % 15746 << '\n';
+    cout << cases[len] << '\n';
 }
