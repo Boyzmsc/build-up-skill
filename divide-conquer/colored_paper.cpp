a06126586dd0da2950e7de13399f2658e7001e84
Boyzmsc <boyzmsc@naver.com>
2021-03-28 23:36:38 +0900

    Update 2630

--- divide-conquer/colored_paper.cpp
+++ divide-conquer/colored_paper.cpp
@@ -9,86 +9,89 @@ int blue = 0;
 
 bool check(int y, int x, int n)
 {
-    int check = paper[y][x];
+  int check = paper[y][x];
 
-    if (n == 1)
-    {
-        if (check == 0)
-        {
-            white++;
-            return true;
-        }
-        else
-        {
-            blue++;
-            return true;
-        }
-    }
-
-    for (int i = y; i < y + n; i++)
-    {
-        for (int j = x; j < x + n; j++)
-        {
-            if (i == y && j == x)
-            {
-                continue;
-            }
-            else
-            {
-                if (check != paper[i][j])
-                {
-                    return false;
-                }
-            }
-        }
-    }
-
+  if (n == 1)
+  {
     if (check == 0)
     {
-        white++;
+      white++;
+      return true;
     }
     else
     {
-        blue++;
+      blue++;
+      return true;
     }
+  }
 
-    return true;
+  for (int i = y; i < y + n; i++)
+  {
+    for (int j = x; j < x + n; j++)
+    {
+      if (i == y && j == x)
+      {
+        continue;
+      }
+      else
+      {
+        if (check != paper[i][j])
+        {
+          return false;
+        }
+      }
+    }
+  }
+
+  if (check == 0)
+  {
+    white++;
+  }
+  else
+  {
+    blue++;
+  }
+
+  return true;
 }
 
 void fold(int y, int x, int n)
 {
-    if (!check(y, x, n))
-    {
-        fold(y, x, n / 2);
-    }
-    if (!check(y, x + n , n))
-    {
-        fold(y, x + n , n / 2);
-    }
-    if (!check(y + n , x, n))
-    {
-        fold(y + n , x, n / 2);
-    }
-    if (!check(y + n , x + n , n))
-    {
-        fold(y + n , x + n , n / 2);
-    }
+  if (!check(y, x, n))
+  {
+    fold(y, x, n / 2);
+  }
+  if (!check(y, x + n, n))
+  {
+    fold(y, x + n, n / 2);
+  }
+  if (!check(y + n, x, n))
+  {
+    fold(y + n, x, n / 2);
+  }
+  if (!check(y + n, x + n, n))
+  {
+    fold(y + n, x + n, n / 2);
+  }
 }
 
 int main()
 {
-    cin >> n;
+  cin >> n;
 
-    for (int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> paper[i][j];
-        }
+      cin >> paper[i][j];
     }
+  }
 
-    fold(0, 0, n/2);
+  if (!check(0, 0, n))
+  {
+    fold(0, 0, n / 2);
+  }
 
-    cout << white << '\n';
-    cout << blue << '\n';
+  cout << white << '\n';
+  cout << blue << '\n';
 }
