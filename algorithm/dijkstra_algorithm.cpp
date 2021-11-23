176ced177d487a9b8b17cb949a85d1aa3b1c494f
MSC <boyzmsc@kookmin.ac.kr>
2021-11-23 21:10:31 +0900

    Update Dijkstra Algorithm

--- algorithm/dijkstra_algorithm.cpp
+++ algorithm/dijkstra_algorithm.cpp
@@ -1,59 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int INF = 1e9;
+
 bool cmp(pair<int, int> a, pair<int, int> b) { return a.second < b.second; }
 
 int solution(int N, vector<vector<int>> road, int K) {
-  // City : Key
-  vector<pair<int, int>> city;
-
-  // Key : Pre-Vertex
-  vector<pair<int, int>> dist;
+  vector<pair<int, bool>> city(N + 1, pair<int, bool>(INF, false));
+  vector<pair<int, int>> dist(N + 1, pair<int, int>(INF, 0));
 
   // Init city
-  city.push_back(make_pair(1, 0));
-  for (int i = 2; i <= N; i++) {
-    city.push_back(make_pair(i, INT_MAX));
-  }
+  city[1].first = 0;
 
   // Init dist
-  dist.push_back(make_pair(0, 1)); // Dummy
-  dist.push_back(make_pair(0, 1));
-  for (int i = 2; i <= N; i++) {
-    dist.push_back(make_pair(INT_MAX, -1));
-  }
+  dist[1].first = 0;
+  dist[1].second = 1;
 
-  // Operation
-  while (city.size() != 0) {
+  int cnt = 1;
+  while (cnt < N) {
     // Get Min City
-    sort(city.begin(), city.end(), cmp);
-    int u = city[0].first;
-    city.erase(city.begin());
+    int chk = INF;
+    int u;
+
+    for (int i = 1; i < city.size(); i++) {
+      if (city[i].first < chk && city[i].second == false) {
+        chk = city[i].first;
+        u = i;
+      }
+    }
+
+    city[u].second = true;
+    cnt++;
 
     for (int i = 0; i < road.size(); i++) {
       if (road[i][0] == u) {
         int v = road[i][1];
-        if (dist[v].first > dist[u].first + road[i][2]) {
+        if (city[v].second == false &&
+            dist[v].first > dist[u].first + road[i][2]) {
           dist[v].first = dist[u].first + road[i][2];
           dist[v].second = u;
 
-          for (int j = 0; j < city.size(); j++) {
-            if (city[j].first == v) {
-              city[j].second = dist[u].first + road[i][2];
-            }
-          }
+          city[v].first = dist[u].first + road[i][2];
         }
       } else if (road[i][1] == u) {
         int v = road[i][0];
-        if (dist[v].first > dist[u].first + road[i][2]) {
+        if (city[v].second == false &&
+            dist[v].first > dist[u].first + road[i][2]) {
           dist[v].first = dist[u].first + road[i][2];
           dist[v].second = u;
 
-          for (int j = 0; j < city.size(); j++) {
-            if (city[j].first == v) {
-              city[j].second = dist[u].first + road[i][2];
-            }
-          }
+          city[v].first = dist[u].first + road[i][2];
         }
       }
     }
