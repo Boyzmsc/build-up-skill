5530f14f650932d44f0a462e20ec4faf86075432
MSC <boyzmsc@kookmin.ac.kr>
2021-11-25 22:46:17 +0900

    Update Dijkstra Algorithm

--- algorithm/dijkstra_algorithm.cpp
+++ algorithm/dijkstra_algorithm.cpp
@@ -1,66 +1,97 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int INF = 1e9;
+int INF = 9999;
 
-bool cmp(pair<int, int> a, pair<int, int> b) { return a.second < b.second; }
+class Edge {
+public:
+  int n[2];
+  int w;
 
-int solution(int N, vector<vector<int>> road, int K) {
-  vector<pair<int, bool>> city(N + 1, pair<int, bool>(INF, false));
-  vector<pair<int, int>> dist(N + 1, pair<int, int>(INF, 0));
+  Edge(int u, int v, int w) {
+    this->n[0] = u;
+    this->n[1] = v;
+    this->w = w;
+  }
+};
 
-  // Init city
-  city[1].first = 0;
+struct compare {
+  bool operator()(pair<int, int> a, pair<int, int> b) {
+    return a.second > b.second;
+  }
+};
 
-  // Init dist
-  dist[1].first = 0;
-  dist[1].second = 1;
+int solution(int n, vector<Edge> road) {
+  int matrix[n][n];
+  memset(matrix, 0, sizeof(matrix));
+  for (int i = 0; i < road.size(); i++) {
+    matrix[road[i].n[0] - 1][road[i].n[1] - 1] = road[i].w;
+  }
 
-  int cnt = 1;
-  while (cnt < N) {
-    // Get Min City
-    int chk = INF;
-    int u;
+  priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+  pq.push(make_pair(0, 0));
+  for (int i = 1; i < n; i++) {
+    pq.push(make_pair(i, INF));
+  }
 
-    for (int i = 1; i < city.size(); i++) {
-      if (city[i].first < chk && city[i].second == false) {
-        chk = city[i].first;
-        u = i;
-      }
+  vector<pair<int, int>> rlt(n, make_pair(INF, INF));
+  rlt[0].first = 0;
+  rlt[0].second = 0;
+
+  bool visited[n] = {
+      false,
+  };
+
+  while (!pq.empty()) {
+    int pos = pq.top().first;
+    int key = pq.top().second;
+    pq.pop();
+
+    if (visited[pos]) {
+      continue;
     }
 
-    city[u].second = true;
-    cnt++;
+    visited[pos] = true;
 
-    for (int i = 0; i < road.size(); i++) {
-      if (road[i][0] == u) {
-        int v = road[i][1];
-        if (city[v].second == false &&
-            dist[v].first > dist[u].first + road[i][2]) {
-          dist[v].first = dist[u].first + road[i][2];
-          dist[v].second = u;
+    for (int i = 0; i < n; i++) {
+      if (matrix[pos][i] == 0 || visited[i] == true) {
+        continue;
+      }
 
-          city[v].first = dist[u].first + road[i][2];
-        }
-      } else if (road[i][1] == u) {
-        int v = road[i][0];
-        if (city[v].second == false &&
-            dist[v].first > dist[u].first + road[i][2]) {
-          dist[v].first = dist[u].first + road[i][2];
-          dist[v].second = u;
+      pq.push(make_pair(i, matrix[pos][i] + key));
 
-          city[v].first = dist[u].first + road[i][2];
-        }
+      if (rlt[i].first > matrix[pos][i] + key) {
+        rlt[i].first = matrix[pos][i] + key;
+        rlt[i].second = matrix[pos][i];
       }
     }
   }
 
   int answer = 0;
-  for (int i = 1; i <= N; i++) {
-    if (dist[i].first <= K) {
-      answer++;
-    }
+  for (int i = 1; i < n; i++) {
+    answer += rlt[i].second;
   }
 
   return answer;
+}
+
+int main() {
+  int test;
+  scanf("%d", &test);
+  while (test--) {
+    vector<Edge> road;
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+      int k, m;
+      scanf("%d %d", &k, &m);
+      for (int j = 0; j < m; j++) {
+        int v, w;
+        scanf("%d %d", &v, &w);
+        road.push_back(Edge(k, v, w));
+      }
+    }
+
+    printf("%d\n", solution(n, road));
+  }
 }
